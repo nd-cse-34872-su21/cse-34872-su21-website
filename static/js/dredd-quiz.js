203a65636b76f5f8a9c4a08e14dfd64c690b4771
Peter Bui <pbui@nd.edu>
2021-05-23 22:43:17 -0400

    dredd-quiz: fix URL

--- static/js/dredd-quiz.js
+++ static/js/dredd-quiz.js
@@ -59,7 +59,7 @@ function submitQuiz(quiz_url) {
 	// first, erase the innerHTML of the JSON box
 	document.getElementById('quiz-responses').innerHTML = ``;
 	var assignment_name = quiz_url.split('/')[2].split('.')[0]
-	var url = 'https://dredd.h4x0r.space/quiz/cse-20289-sp21/' + assignment_name;
+	var url = 'https://dredd.h4x0r.space/quiz/cse-34872-su21/' + assignment_name;
 	fetch(url, {
 		body: JSON.stringify(responses),
 		method: 'POST'
